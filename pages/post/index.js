e889fb4bc8626cb0a13e5044d5c600acfff1f674
David Fales <davidfalesct@gmail.com>
2022-12-31 15:29:26 -0700

    post error fix

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,12 +1,53 @@
-import { router } from 'next/router'
-import React from 'react'
+import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
+import { router, useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
 function Post() {
 
-    const { userId, id } = router.query;
+    const supabase = useSupabaseClient()
+  const user = useUser()
+  const router = useRouter()
+  const [loading, setLoading] = useState(true)
+  const [fetchError, setFetchError] = useState(null)
+  const [avatar_url, setAvatarUrl] = useState(null)
+  const [full_name, setFullName] = useState(null)
+  const [username, setUsername] = useState(null)
 
-    console.log(userId)
-    console.log(id)
+  useEffect(() => {
+    if(router.isReady){
+        const { userId, id } = router.query;
+        getProfile(id, userId)
+    }
+  }, [router.isReady])
+
+  async function getProfile( id, userId ) {
+    try {
+      setLoading(true)
+
+      const { data, error, status } = await supabase
+        .from('profiles')
+        .select("*")
+        .eq('id', userId)
+        .single()
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setAvatarUrl(data.avatar_url)
+        setFullName(data.full_name)
+        setUsername(data.username)
+       }
+    } catch (error) {
+     alert('Error loading user data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+      console.log(id)
+        console.log(userId)
+    }
+}
 
   return (
     <div>
