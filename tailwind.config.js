e86cb87a88af4a1770d49c16c572b74d8f184e5f
David Fales <davidfalesct@gmail.com>
2023-01-02 17:50:57 -0700

    styling fix one

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'xs': '475px',
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  variants:{
     extend: {},
   },
   plugins: [],
